cd5969eeff61d431bfe99beaf1e7a9481b73dcb6
nyugalkishore2001 <nyugalkishore@gmail.com>
2025-02-11 16:05:13 -0500

    level 2 calculator

--- tests/test_calcoperations.py
+++ tests/test_calcoperations.py
@@ -1,22 +1,22 @@
 '''My Calculator Test'''
-from calculator import Calculator
+from calculator.CalcOperations import add,subtract,multiply,divide
 
 def test_addition():
     '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    assert add(2,2) == 4
 
 def test_subtraction():
     '''Test that subtraction function works '''    
-    assert Calculator.subtract(2,2) == 0
+    assert subtract(2,2) == 0
 
 def test_multiplication():
     '''Test that multiplication function works '''    
-    assert Calculator.multiply(2,2) == 4
+    assert multiply(2,2) == 4
 
 def test_division():
     '''Test that division function works '''    
-    assert Calculator.divide(2,2) == 1
+    assert divide(2,2) == 1
 
 def test_division_by_zero():
-    '''Test that division function works '''
-    assert Calculator.divide(2,0) == "Error : Division by zero"
+    '''Test that division function works '''    
+    assert divide(2,0) == "Error : Division by zero"
